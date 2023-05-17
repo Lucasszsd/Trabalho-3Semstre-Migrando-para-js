dade79a2cd4f12f0357dcc3ee8fa10dc5855d956
Lucas Dutra <lucas_dbraga@hotmail.com>
2023-05-17 11:33:43 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+let tentativaDeLogin = 3;
+
+
+function logar() {
+    let login = document.getElementById("login").value;
+    let senha = document.getElementById("senha").value;
+    let cargo = document.getElementsByName("cargo")[0].value;
+    verificaLogin(cargo,senha,login);
+}
+
+function verificaLogin(cargo, senha, login){
+    if (cargo === "administrador" && senha === "admin" && login === "admin") {
+        window.open("http://www.maringa.pr.gov.br/site/", "_blank");
+    }else{
+        alert("Login ou senha incorretos " + (tentativaDeLogin-1) + " Tentativas restantes");
+        tentativaDeLogin--;
+        if(tentativaDeLogin === 0){
+            alert("Conta bloqueada");
+        }
+    }
+
+    console.log(tentativaDeLogin);
+}
