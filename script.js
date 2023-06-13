455347f24fb52062400e7204be742b8a1a35c3f3
Lucas Dutra <lucas_dbraga@hotmail.com>
2023-06-13 09:11:41 -0600

    fechar pagina com bloqueio da conta

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ function verificaLogin(senha, login){
         tentativaDeLogin--;
         if(tentativaDeLogin === 0){
             alert("Conta bloqueada");
+            window.close();
         }
     }
 
